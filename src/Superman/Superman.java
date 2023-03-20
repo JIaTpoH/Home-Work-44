3803d3b1c15d1caa4c3e6d7355d4269ab688e1f3
Dmitri Mandaji <badmopss@gmail.com>
2023-03-20 21:20:11 +0100

    add the interface and class

---
+++ src/Superman/Superman.java
@@ -0,0 +1,8 @@
+package Superman;
+// Напишите интерфейсы
+// "Умеет летать" CanFly с методом fly(),
+// "Умеет бегать" CanRun с методом run
+// "умеет плавать" CanSwim с методом swim
+//  реализуйте их в классе "Супермен" Superman.
+public class Superman {
+}
